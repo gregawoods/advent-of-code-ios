c087c409e925bc224ff220a43730d30b3c1f42d0
Greg Woods <greg.woods@moserit.com>
2020-12-02 20:05:11 -0500

    Initial Commit

---
+++ AdventOfCode2020/AdventOfCode2020App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2020App.swift
+//  AdventOfCode2020
+//
+//  Created by Greg Woods on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2020App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventOfCode2020/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdventOfCode2020
+//
+//  Created by Greg Woods on 12/2/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
