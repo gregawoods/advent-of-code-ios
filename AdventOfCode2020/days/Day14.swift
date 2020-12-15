74477523d6eb1f47baaa213ce07288413aca8309
Greg Woods <greg.woods@moserit.com>
2020-12-14 22:01:03 -0500

    day 14 part 2

--- AdventOfCode2020/days/Day14.swift
+++ AdventOfCode2020/days/Day14.swift
@@ -9,23 +9,39 @@ import Foundation
 
 struct Day14: DayProtocol {
 
-    struct Computer {
+    struct Line {
+        var task: String
+        var left: Int
+        var right: String
+    }
+
+    class Computer {
         var mask: String = ""
         var memory: [Int: Int] = [:]
         var sum: Int {
             return Array(memory.values).sum()
         }
 
-        mutating func process(line: String) {
+        func process(line: String) {
+            let parsed = parseLine(line: line)
+
+            if parsed.task == "mask" {
+                self.mask = parsed.right
+            } else {
+                self.memory[parsed.left] = translatedValue(value: Int(parsed.right)!)
+            }
+        }
+
+        func parseLine(line: String) -> Line {
             let parts = line.components(separatedBy: " = ")
 
             if parts[0] == "mask" {
-                self.mask = parts[1]
+                return Line(task: "mask", left: 0, right: parts[1])
             } else {
-                let address = Int(
+                let left = Int(
                     parts[0].replacingOccurrences(of: "mem[", with: "").replacingOccurrences(of: "]", with: "")
                 )!
-                self.memory[address] = translatedValue(value: Int(parts[1])!)
+                return Line(task: "mem", left: left, right: parts[1])
             }
         }
 
@@ -54,7 +70,7 @@ struct Day14: DayProtocol {
     }
 
     func calculatePart1(_ input: [String]) -> Int {
-        var computer = Computer()
+        let computer = Computer()
 
         for line in input {
             computer.process(line: line)
@@ -63,7 +79,75 @@ struct Day14: DayProtocol {
         return computer.sum
     }
 
+    class ComputerV2: Computer {
+        override func process(line: String) {
+            let parsed = parseLine(line: line)
+
+            if parsed.task == "mask" {
+                self.mask = parsed.right
+            } else {
+                let processed = preProcessMask(value: parsed.left)
+                let addresses = maskToMemoryAddresses(inputMask: processed)
+                let intVal = Int(parsed.right)!
+                addresses.forEach { memory[$0] = intVal }
+            }
+        }
+
+        func preProcessMask(value: Int) -> String {
+            var binaryDigits = Array(String(value, radix: 2))
+
+            repeat {
+                binaryDigits.insert("0", at: 0)
+            } while binaryDigits.count < 36
+
+            var newDigits: [String] = []
+            var index = 0
+
+            for maskDigit in mask {
+                if maskDigit == "X" || maskDigit == "1" {
+                    newDigits.append(String(maskDigit))
+                } else {
+                    newDigits.append(String(binaryDigits[index]))
+                }
+                index += 1
+            }
+
+            return newDigits.joined()
+        }
+
+        func maskToMemoryAddresses(inputMask: String) -> [Int] {
+            var digitArrays: [[String]] = []
+            digitArrays.append([])
+
+            for maskDigit in inputMask {
+                if maskDigit == "X" {
+                    let len = digitArrays.count
+                    for n in 0...len-1 {
+                        digitArrays[n].append("0")
+                    }
+                    for n in 0...len-1 {
+                        var copy = digitArrays[n]
+                        copy[copy.count-1] = "1"
+                        digitArrays.append(copy)
+                    }
+                } else {
+                    for n in 0...digitArrays.count-1 {
+                        digitArrays[n].append(String(maskDigit))
+                    }
+                }
+            }
+
+            return digitArrays.map { Int($0.joined(), radix: 2)! }
+        }
+    }
+
     func calculatePart2(_ input: [String]) -> Int {
-        return 0
+        let computer = ComputerV2()
+
+        for line in input {
+            computer.process(line: line)
+        }
+
+        return computer.sum
     }
 }
--- AdventOfCode2020Tests/Day14Tests.swift
+++ AdventOfCode2020Tests/Day14Tests.swift
@@ -11,15 +11,15 @@ import XCTest
 class Day14Tests: XCTestCase {
 
     let day = Day14()
-    let sample = [
-        "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
-        "mem[8] = 11",
-        "mem[7] = 101",
-        "mem[8] = 0"
-    ]
     let input = FileReader(file: "day14").lines
 
     func testPart1Sample() {
+        let sample = [
+            "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+            "mem[8] = 11",
+            "mem[7] = 101",
+            "mem[8] = 0"
+        ]
         let result = day.calculatePart1(sample)
         XCTAssertEqual(result, 165)
     }
@@ -31,13 +31,19 @@ class Day14Tests: XCTestCase {
     }
 
     func testPart2Sample() {
+        let sample = [
+            "mask = 000000000000000000000000000000X1001X",
+            "mem[42] = 100",
+            "mask = 00000000000000000000000000000000X0XX",
+            "mem[26] = 1"
+        ]
         let result = day.calculatePart2(sample)
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 208)
     }
 
     func testPart2() throws {
         let result = day.calculatePart2(input)
         print("Day 14 Part 2: \(result)")
-        XCTAssertEqual(result, 0)
+        XCTAssertEqual(result, 3816594901962)
     }
 }
