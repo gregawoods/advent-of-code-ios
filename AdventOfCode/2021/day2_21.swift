92e4db58f4a99d00cda7d55d624f70c514443667
Greg Woods <greg.woods@moserit.com>
2021-12-02 00:16:24 -0500

    day2

--- AdventOfCode/2021/day2_21.swift
+++ AdventOfCode/2021/day2_21.swift
@@ -9,11 +9,73 @@ import Foundation
 
 struct Y21_Day2 : DayProtocol {
 
-    func part1(_ input: [Int]) -> Int {
-        return 0
+    enum Dir {
+        case forward
+        case up
+        case down
     }
     
-    func part2(_ input: [Int]) -> Int {
-        return 0
+    struct Step {
+        var amount = 0
+        var dir: Dir = .forward
+    }
+    
+    fileprivate func parseStep(stepStr: String) -> Step {
+        let parts = stepStr.components(separatedBy: " ")
+        let amount = Int(parts[1])!
+        
+        if parts[0] == "forward" {
+            return Step(amount: amount, dir: .forward)
+        } else if parts [0] == "up" {
+            return Step(amount: amount, dir: .up)
+        } else {
+            return Step(amount: amount, dir: .down)
+        }
+    }
+    
+    func part1(_ input: [String]) -> Int {
+        var x = 0
+        var y = 0
+        
+        let steps = input.map({ str in
+            return parseStep(stepStr: str)
+        })
+        
+        for step in steps {
+            switch step.dir {
+            case .forward:
+                x += step.amount
+            case .down:
+                y += step.amount
+            case .up:
+                y -= step.amount
+            }
+        }
+        
+        return x * y
+    }
+    
+    func part2(_ input: [String]) -> Int {
+        var x = 0
+        var y = 0
+        var aim = 0
+        
+        let steps = input.map({ str in
+            return parseStep(stepStr: str)
+        })
+        
+        for step in steps {
+            switch step.dir {
+            case .forward:
+                x += step.amount
+                y += (step.amount * aim)
+            case .down:
+                aim += step.amount
+            case .up:
+                aim -= step.amount
+            }
+        }
+        
+        return x * y
     }
 }
--- AdventOfCodeTests/2021/Y21Day2Tests.swift
+++ AdventOfCodeTests/2021/Y21Day2Tests.swift
@@ -10,13 +10,30 @@ import XCTest
 
 class Y21Day2Tests: XCTestCase {
 
-    let input = FileReader(file: "day2_21").arrayOfInts
+    let sample = [
+        "forward 5",
+        "down 5",
+        "forward 8",
+        "up 3",
+        "down 8",
+        "forward 2"
+    ]
 
+    let input = FileReader(file: "day2_21").lines
+
+    func testPart1Sample() {
+        XCTAssertEqual(Y21_Day2().part1(sample), 150)
+    }
+    
     func testPart1() throws {
-        XCTAssertEqual(Y21_Day2().part1(input), 1)
+        XCTAssertEqual(Y21_Day2().part1(input), 2322630)
+    }
+    
+    func testPart2Sample() {
+        XCTAssertEqual(Y21_Day2().part2(sample), 900)
     }
 
     func testPart2() throws {
-        XCTAssertEqual(Y21_Day2().part2(input), 1)
+        XCTAssertEqual(Y21_Day2().part2(input), 2105273490)
     }
 }
