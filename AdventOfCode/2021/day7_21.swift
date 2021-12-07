0fe5ae8a81a985d204a2cb4cdf2300ba4b3b7dde
Greg Woods <greg.woods@moserit.com>
2021-12-07 09:37:45 -0500

    Formtting and clean up

--- AdventOfCode/2021/day7_21.swift
+++ AdventOfCode/2021/day7_21.swift
@@ -10,30 +10,32 @@ import Foundation
 struct Y21Day7: DayProtocol {
 
     func part1(_ input: [Int]) -> Int {
-        var costs: [Int: Int] = [:]
+        var costs: [Int] = []
 
-        for target in (input.min()!...input.max()!) {
-            costs[target] = input.map { abs(target - $0) }.sum()
+        for target in input.min()!...input.max()! {
+            costs.append(input.map { abs(target - $0) }.sum())
         }
 
-        return costs.values.sorted()[0]
+        return costs.sorted()[0]
     }
 
     func part2(_ input: [Int]) -> Int {
-        var costs: [Int: Int] = [:]
+        var costs: [Int] = []
         let cache = Cache<Int, Int>()
 
-        for target in (input.min()!...input.max()!) {
-            costs[target] = input.map { n in
-                let moves = abs(target - n)
-                return cache.fetch(key: moves) {
-                    var newValue = 0
-                    for n in 0...moves { newValue += n }
-                    return newValue
-                }
-            }.sum()
+        for target in input.min()!...input.max()! {
+            costs.append(
+                input.map { n in
+                    let moves = abs(target - n)
+                    return cache.fetch(key: moves) {
+                        var newValue = 0
+                        for n in 0...moves { newValue += n }
+                        return newValue
+                    }
+                }.sum()
+            )
         }
 
-        return costs.values.sorted()[0]
+        return costs.sorted()[0]
     }
 }
